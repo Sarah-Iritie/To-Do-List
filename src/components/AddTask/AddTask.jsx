f3c86d342049fbb23f78a84702e79b92b7b1c76d
Sarah-Iritie <143420861+Sarah-Iritie@users.noreply.github.com>
2024-03-15 03:32:00 +0100

    responsive

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import './AddTask.scss';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function AddTask({ newTask }) {
   const [inputValue, setInputValue] = useState('');
@@ -17,9 +19,13 @@ export default function AddTask({ newTask }) {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
         className="form-input"
+        placeholder="Add your text"
       />
       <button type="submit" className="form-btn">
         Add
+      </button>
+      <button type="submit" className="plus-icon">
+        <FontAwesomeIcon icon={faCirclePlus} className="add-icon" />
       </button>
     </form>
   );
